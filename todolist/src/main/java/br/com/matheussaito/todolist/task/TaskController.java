1704110a111b1546ecb82b0e5f6ff1df0502a9ae
Matheus Saito <saitomatheus07@gmail.com>
2025-09-16 19:30:58 -0300

    Implementação no método UPTADE para atualizar apenas os campos que o usuario desejar, sem "apagar" os campos que nao foram atualizados

--- todolist/src/main/java/br/com/matheussaito/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/matheussaito/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.matheussaito.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -54,9 +55,8 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setId((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(taskModel, task);
+        return this.taskRepository.save(task);      
     }
 }
